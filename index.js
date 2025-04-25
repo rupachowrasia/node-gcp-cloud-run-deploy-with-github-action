02d18796a4101486c7c80e55e1cb962f1c3feac7
rupachowrasia <rupa.chowrasia@gmail.com>
2025-04-25 14:54:26 +0530

    Small change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = Express();
 const PORT = process.env.PORT || 8080; // don't forget to set env variable
 
 app.get('/', (req, res) =>{
-    res.send('Deploy Node.js app in Cloud Run with Github Action');
+    res.send('Deployed Node.js app in Cloud Run with Github Action');
 })
 
 app.listen(PORT, () =>{
